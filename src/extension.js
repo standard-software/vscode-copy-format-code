c48dadd3a602ec01e95f34929253d023b6e3e70c
Satoshi Yamamoto <standard.software.net@gmail.com>
2024-05-23 19:42:35 +0900

    run test

---
+++ src/extension.js
@@ -0,0 +1,23 @@
+const vscode = require('vscode');
+
+const {
+  registerCommand,
+} = require(`./lib/libVSCode.js`);
+
+function activate(context) {
+
+  registerCommand(context,
+    `vscode-copy-format-code.Hello`,
+    () => {
+      vscode.window.showInformationMessage(`Hello`);
+    }
+  );
+
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+}
