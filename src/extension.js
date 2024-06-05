227e3e81c28b759126f6a70c5a37f3665711e152
Satoshi Yamamoto <standard.software.net@gmail.com>
2024-06-05 11:44:49 +0900

    refactoring getText

--- src/extension.js
+++ src/extension.js
@@ -23,10 +23,102 @@ const {
   _paddingFirst,
 } = require(`./parts/parts.js`);
 
+const assert = (condition, message) => {
+  if (!condition) {
+    throw new Error(message || `Assertion failed`);
+  }
+};
+
+
 const getCopyFormatDataArray = () => {
   const formatData = vscode.workspace
     .getConfiguration(`CopyFormatCode`).get(`CopyFormat`);
   return formatData;
+};
+
+const getLineBreak = (editor) => {
+  const { eol } = editor.document;
+  if (eol === vscode.EndOfLine.LF) {
+    return `\n`;
+  } else if (eol === vscode.EndOfLine.CRLF) {
+    return `\r\n`;
+  }
+  assert(false, `getLineBreak:eol${eol}`);
+};
+
+
+const driveLetterUpper = path => {
+  if (path[1] === `:`) {
+    return path[0].toUpperCase() + path.slice(1);
+  }
+};
+
+const replaceHeaderFooter = (editor, text) => {
+  if (!text) {
+    return ``;
+  }
+
+  const lineBreak = getLineBreak(editor);
+  const startLine = __min(editor.selections.map(s=>s.start.line)) + 1;
+  const endLine = __max(editor.selections.map(s=>s.end.line)) + 1;
+
+  text = text.replaceAll(`%FilePathFull%`,
+    // editor.document.fileName
+    driveLetterUpper(editor.document.uri.fsPath)
+  );
+  text = text.replaceAll(`%FilePathFullSlash%`,
+    driveLetterUpper(
+      _excludeFirst(editor.document.uri.path, `/`)
+    )
+  );
+
+  let workspaceFolder = vscode.workspace.getWorkspaceFolder(editor.document.uri);
+  if (workspaceFolder) {
+    const relativePath = path.relative(
+      workspaceFolder.uri.fsPath,
+      editor.document.uri.fsPath
+    );
+    text = text.replaceAll(`%FilePathRelative%`,
+      relativePath
+    );
+    text = text.replaceAll(`%FilePathRelativeSlash%`,
+      relativePath.replaceAll(`\\`, `/`)
+    );
+
+    const projectName = path.basename(workspaceFolder.uri.path);
+    text = text.replaceAll(`%ProjectName%`,
+      projectName
+    );
+  }
+
+  text = text.replaceAll(`%FileName%`,
+    path.basename(editor.document.uri.path)
+  );
+  text = text.replaceAll(`%FileExt%`,
+    _excludeFirst(path.extname(editor.document.uri.path), `.`)
+  );
+  text = text.replaceAll(`%FileNameWithoutExt%`,
+    _subFirstDelimFirst(path.basename(editor.document.uri.path), `.`)
+  );
+  text = text.replaceAll(`%ParentFolderName%`,
+    path.basename(path.dirname(editor.document.uri.path))
+  );
+
+  text = text.replaceAll(`%LineNumberStart%`,
+    `${startLine}`
+  );
+  text = text.replaceAll(`%LineNumberStartZeroPad%`,
+    _paddingFirst(`${startLine}`, `${endLine}`.length, `0`)
+  );
+  text = text.replaceAll(`%LineNumberEnd%`,
+    `${endLine}`
+  );
+
+  text = text.replaceAll(`¥r¥n`, `¥n`);
+  text = text.replaceAll(`¥n`, lineBreak);
+  text += lineBreak;
+
+  return text;
 };
 
 const copyCode = (format) => {
@@ -36,82 +128,61 @@ const copyCode = (format) => {
     return;
   }
 
-  const driveLetterUpper = path => {
-    if (path[1] === `:`) {
-      return path[0].toUpperCase() + path.slice(1);
+  const header = replaceHeaderFooter(editor, format.header);
+  const footer = replaceHeaderFooter(editor, format.footer);
+
+  const getText = (editor, option) => {
+    const lineBreak = getLineBreak(editor);
+
+    assert([`none`, `file`, `startOne`].includes(option.lineNumber));
+
+    const startLine = __min(editor.selections.map(s=>s.start.line)) + 1;
+    const endLine = __max(editor.selections.map(s=>s.end.line)) + 1;
+
+    console.log(`extension.js 142`, startLine, endLine);
+
+    let numberDigit = 0;
+    if (option.lineNumber === `file`) {
+      numberDigit = endLine.toString().length;
+    } else if (option.lineNumber === `startOne`) {
+      numberDigit = (endLine - startLine + 1).toString().length;
     }
+
+    let result = ``;
+    for (const [selectionIndex, { start, end }] of editor.selections.entries()) {
+      for (let i = start.line; i <= end.line; i += 1) {
+        if (
+          start.line !== end.line &&
+          i === end.line &&
+          end.character === 0
+        ) {
+          break;
+        }
+
+        const lineText = editor.document.lineAt(i).text;
+        if (option.lineNumber === `none`) {
+          result += `${lineText}${lineBreak}`;
+        } else if (option.lineNumber === `file`) {
+          const lineNumber =
+            (i + 1).toString().padStart(numberDigit, `0`);
+          result += `${lineNumber}: ${lineText}${lineBreak}`;
+        } else if (option.lineNumber === `startOne`) {
+          const lineNumber =
+            (i + 1 - startLine + 1).toString().padStart(numberDigit, `0`);
+          result += `${lineNumber}: ${lineText}${lineBreak}`;
+        }
+      }
+      if (selectionIndex !== editor.selections.length - 1) {
+        result += lineBreak;
+      }
+    }
+    return result;
   };
 
-  const replaceHeaderFooter = (text, startLine, endLine) => {
-    if (!text) {
-      return ``;
-    }
-    text = text.replaceAll(`%FilePathFull%`,
-      // editor.document.fileName
-      driveLetterUpper(editor.document.uri.fsPath)
-    );
-    text = text.replaceAll(`%FilePathFullSlash%`,
-      driveLetterUpper(
-        _excludeFirst(editor.document.uri.path, `/`)
-      )
-    );
-
-    let workspaceFolder = vscode.workspace.getWorkspaceFolder(editor.document.uri);
-    if (workspaceFolder) {
-      const relativePath = path.relative(
-        workspaceFolder.uri.fsPath,
-        editor.document.uri.fsPath
-      );
-      text = text.replaceAll(`%FilePathRelative%`,
-        relativePath
-      );
-      text = text.replaceAll(`%FilePathRelativeSlash%`,
-        relativePath.replaceAll(`\\`, `/`)
-      );
-
-      const projectName = path.basename(workspaceFolder.uri.path);
-      text = text.replaceAll(`%ProjectName%`,
-        projectName
-      );
-    }
-
-    text = text.replaceAll(`%FileName%`,
-      path.basename(editor.document.uri.path)
-    );
-    text = text.replaceAll(`%FileExt%`,
-      _excludeFirst(path.extname(editor.document.uri.path), `.`)
-    );
-    text = text.replaceAll(`%FileNameWithoutExt%`,
-      _subFirstDelimFirst(path.basename(editor.document.uri.path), `.`)
-    );
-    text = text.replaceAll(`%ParentFolderName%`,
-      path.basename(path.dirname(editor.document.uri.path))
-    );
-
-    text = text.replaceAll(`%LineNumberStart%`,
-      `${startLine}`
-    );
-    text = text.replaceAll(`%LineNumberStartZeroPad%`,
-      _paddingFirst(`${startLine}`, `${endLine}`.length, `0`)
-    );
-    text = text.replaceAll(`%LineNumberEnd%`,
-      `${endLine}`
-    );
-
-    text += `\n`;
-
-    return text;
-  };
-
-
-  const startLine = __min(editor.selections.map(s=>s.start.line)) + 1;
-  const endLine = __max(editor.selections.map(s=>s.end.line)) + 1;
-  let header = replaceHeaderFooter(format.header, startLine, endLine);
-  const footer = replaceHeaderFooter(format.footer, startLine, endLine);
 
   let body = ``;
   if (!format.option) {
-    body = getTextNoLineNumberNoFormat(editor);
+    body = getText(editor,  { lineNumber: `none` });
   } else {
     const { deleteIndent, deleteBlankLine, lineNumber } = format.option;
     if (!lineNumber) {
@@ -122,7 +193,7 @@ const copyCode = (format) => {
       } else if (deleteBlankLine) {
         body = getTextNoLineNumberDeleteBlankLine(editor);
       } else {
-        body = getTextNoLineNumberNoFormat(editor);
+        body = getText(editor, { lineNumber: `none` });
       }
     } else {
       if (deleteIndent && deleteBlankLine) {
@@ -132,7 +203,7 @@ const copyCode = (format) => {
       } else if (deleteBlankLine) {
         body = getTextLineNumberDeleteBlankLine(editor);
       } else {
-        body = getTextLineNumberNoFormat(editor);
+        body = getText(editor, { lineNumber: `file` });
       }
     }
   }
